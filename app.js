5a42a8f7751668424e8a2f0e7f5034cf05eaa986
MarekPouchly <marekpouchl8@gmail.com>
2023-02-23 12:14:12 +0100

    Create Player object and modules.

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
+const Player = (name, square) => {
+
+}
+
 const gameboard = (() => {
-    const gameboard = [];
+    const board = [];
+    for( let i = 0; i < 9; i++) {
+        board.push('X');
+    }
+})();
+
+const displayController = (() => {
+    const squares = document.querySelectorAll('.square');
+    
+    squares.forEach( square => {
+        square.addEventListener('click', (event) => {
+            console.log(event.target.dataset.index)
+        })
+    })
 })();
