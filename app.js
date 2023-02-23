a1bfdf8b078f486a85121a93884aa2e6932afe94
MarekPouchly <marekpouchl8@gmail.com>
2023-02-23 14:47:16 +0100

    Allow players to add marks to specific spot.

--- app.js
+++ app.js
@@ -1,34 +1,82 @@
-const Player = (name, square) => {
+const Player = (sign) => {
+    this.sign = sign;
 
+    const getSign = () => {
+        return sign;
+    }
+
+    return { getSign };
 }
 
 const gameboard = (() => {
     const board = [];
+
     for( let i = 0; i < 9; i++) {
-        board.push('X');
-    }
+        board.push('');
+    };
 
-    const getSymbol = (index) => {
+    const getSquare = (index) => {
         return board[index];
+    };
+
+    const setSquare = (index, symbol) => {
+        board[index] = symbol;
     }
 
-    return {getSymbol};
+    const resetGrid = () => {
+        for( let i = 0; i < 9; i++) {
+            board[i] = '';
+        };
+    };
+
+    return {getSquare, setSquare, resetGrid};
 
 })();
 
 const displayController = (() => {
     const squares = document.querySelectorAll('.square');
+    const restartButton = document.querySelector('.restartBtn');
     
     squares.forEach( square => {
         square.addEventListener('click', (event) => {
-            console.log(event.target.dataset.index)
-            renderContent();
+            if ( gameboard.getSquare(event.target.dataset.index) !== '' ) {
+                return
+            }
+
+            game.playRound(event.target.dataset.index)
+            renderSquareContent();
         })
     })
 
-    const renderContent = () => {
+    restartButton.addEventListener('click', () => {
+        gameboard.resetGrid();
+        renderSquareContent();
+    });
+
+    const renderSquareContent = () => {
         for ( let i = 0; i < squares.length; i++) {
-            squares[i].textContent = gameboard.getSymbol(i);
+            squares[i].textContent = gameboard.getSquare(i);
         }
-    }; 
+    };
+})();
+
+const game = (() => {
+    const playerX = Player("X");
+    const playerO = Player("O");
+    let round = 1;
+
+    const playRound = (index) => {
+        gameboard.setSquare(index, getPlayerSign());
+        round++;
+    }
+
+    const getPlayerSign = () => {
+        if (round % 2 === 1) {
+            return playerX.getSign();
+        } else {
+            return playerO.getSign();
+        }
+    }
+
+    return {playRound};
 })();
